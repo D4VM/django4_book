a7842cd6e14b5defa2c5febd5ebf2798a37121cf
D4VM <davit.miru@gmail.com>
2024-02-01 15:28:00 +0400

    paginations views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,42 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    # получаем QuerySet со списком наших постов
+    post_list = Post.published.all()
+
+    # раздробляем этот список по 5 обьекта(поста) на страницу
+    paginator = Paginator(post_list, 5)
+
+    # получаем данные из GET запроса с парамтром page, или используем 1 как дефолтное значение
+    page_number = request.GET.get("page", 1)
+
+    try:
+        # передаем в posts список постов с конкретной страницой через page_number
+        posts = paginator.page(page_number)
+
+    except PageNotAnInteger:
+        # если в GET запросе не цифра на строка то вернуть 1 страницу
+        posts = paginator.page(1)
+        
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+        slug=post,
+    )
     return render(request, "blog/post/detail.html", {"post": post})
