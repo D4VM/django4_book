31a2b063b1b4f75b7e2999aa974691da1d749a17
D4VM <davit.miru@gmail.com>
2024-02-01 15:38:48 +0400

    commenting importnat info

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,15 +4,31 @@ from django.views.generic import ListView
 
 from .models import Post
 
+
 class PostListView(ListView):
     """
     Альтернативное представление списка постов
     """
 
+    # атрибут queryset используется для того, чтобы иметь конкретно-при-
+    # кладной набор запросов QuerySet, не извлекая все объекты. Вместо
+    # определения атрибута queryset мы могли бы указать model=Post, и Django
+    # сформировал бы для нас типовой набор запросов Post.objects.all();
     queryset = Post.published.all()
-    context_object_name = 'posts'
+
+    # контекстная переменная posts используется для результатов запроса.
+    # Если не указано имя контекстного объекта context_object_name, то по
+    # умолчанию используется переменная object_list;
+    context_object_name = "posts"
+
+    # в атрибуте paginate_by задается постраничная разбивка результатов
+    # с возвратом трех объектов на страницу;
     paginate_by = 3
-    template_name = 'blog/post/list.html'
+
+    # конкретно-прикладной шаблон используется для прорисовки страницы
+    # шаблоном template_name. Если шаблон не задан, то по умолчанию List-
+    # View будет использовать blog/post_list.html
+    template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, post):
