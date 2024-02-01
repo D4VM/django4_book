d94b1e12f428f0c97bbbc3aa6df0fcf87338f85b
D4VM <davit.miru@gmail.com>
2024-02-01 15:35:38 +0400

    PostListView

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,33 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 from .models import Post
 
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
 
-def post_list(request):
-    # получаем QuerySet со списком наших постов
-    post_list = Post.published.all()
-
-    # раздробляем этот список по 5 обьекта(поста) на страницу
-    paginator = Paginator(post_list, 5)
-
-    # получаем данные из GET запроса с парамтром page, или используем 1 как дефолтное значение
-    page_number = request.GET.get("page", 1)
-
-    try:
-        # передаем в posts список постов с конкретной страницой через page_number
-        posts = paginator.page(page_number)
-
-    except PageNotAnInteger:
-        # если в GET запросе не цифра на строка то вернуть 1 страницу
-        posts = paginator.page(1)
-        
-    except EmptyPage:
-        # Если page_number находится вне диапазона, то
-        # выдать последнюю страницу
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request, "blog/post/list.html", {"posts": posts})
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
