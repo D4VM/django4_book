1207a414456f1963c52fec677dee9c434299a68b
D4VM <davit.miru@gmail.com>
2024-02-02 12:57:17 +0400

    fixed imports

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.template.defaulttags import comment
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
